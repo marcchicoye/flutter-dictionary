a127bc2af58abcccc1147029cb8eae68e6540686
marcchicoye <marcchicoye@gmail.com>
2025-06-06 19:31:40 -0400

    implement the reading of json file

---
+++ lib/model/word.dart
@@ -0,0 +1,5 @@
+class Word {
+  const Word(this.word, this.definition);
+  final String word;
+  final String definition;
+}
--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class QuizScreen extends StatefulWidget {
   const QuizScreen({super.key});
@@ -10,8 +13,46 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreen extends State<QuizScreen> {
+  _QuizScreen() {
+    readJson();
+  }
+  List<Map<String, String>> _dictionary = [];
+
+  Future<void> readJson() async {
+    final String response =
+        await rootBundle.loadString('assets/data/vocabulary.json');
+
+    final List<dynamic> data = json.decode(response);
+
+    // Caster chaque élément en Map<String, String>
+    setState(() {
+      _dictionary = data
+          .map<Map<String, String>>((item) => {
+                'mot': item['mot'] as String,
+                'definition': item['definition'] as String
+              })
+          .toList();
+    });
+
+    print(_dictionary[0]); // Pour vérification
+  }
+
   @override
   Widget build(Object context) {
-    return Text('Quiz Screen');
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Text('Definition'),
+              Text('mot 1'),
+              Text('mot 2'),
+              Text('mot 3'),
+              Text('mot 4'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
